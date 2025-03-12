61b9261a6d64c4a49818aa026e9a840194c3fdcc
tanmayddani <97046812+tanmayddani@users.noreply.github.com>
2025-03-12 12:33:13 +0000

    added math file

--- 01_Basics/04_comparison.js
+++ 01_Basics/04_comparison.js
@@ -1,3 +1,20 @@
 console.log(2>1)
 console.log("2"===1)
 console.log("seing"===12)
+
+let myname="HimanshuChoudhry"
+let anothername="myname"
+anothername = "chia or code"
+console.log(anothername)
+console.log(myname)
+
+let user1 = {
+    email:"user@gmail.com",
+    upi:"user1@ybl",
+}
+
+let user2 = user1;
+
+user2.emal ="userjhvbhbhb@kfbkjrvf.com"
+console.log(user1.email)
+console.log(user2.email)
---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,10 @@
+const name ="Hitesh"
+let repocount = 46545621;
+
+console.log(`my name is ${name} and my repo count is ${repocount}thanks`)
+
+const gamename = "hitesh";  // Using a string literal directly
+console.log(gamename[0]);  // This will correctly log 'h'
+console.log(gamename.length); 
+console.log(gamename.toUpperCase()); 
+console.log(gamename.charAt('5')); 
